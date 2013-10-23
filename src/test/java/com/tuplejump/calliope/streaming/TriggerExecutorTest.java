bf04041a5474dba01dc44180a595addffd31859f
Suresh Sajja <mail@sureshsajja.com>
2013-10-23 13:41:43 +0530

    adding tests

---
+++ src/test/java/com/tuplejump/calliope/streaming/TriggerExecutorTest.java
@@ -0,0 +1,13 @@
+package com.tuplejump.calliope.streaming;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class TriggerExecutorTest {
+
+
+
+}
